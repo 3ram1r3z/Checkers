f232f7bc0f186e587f0c20ddd97d58af07a7b0af
Emmanuel Ramirez <emmanuel.ramirez@weightwatchers.com>
2019-06-21 11:18:41 -0700

    Initial Commit

---
+++ Checkers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checkers
+//
+//  Created by Emmanuel Ramirez on 6/21/19.
+//  Copyright © 2019 Emmanuel Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
